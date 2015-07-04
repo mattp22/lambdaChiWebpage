61d7dd6a5af4a50a05a7b9b72f0ce4fc1692686f
Matt Preziotti <mattp22@vt.edu>
2015-07-04 15:04:54 -0400

    initial

---
+++ public/app/app.module.js
@@ -0,0 +1,28 @@
+(function() {
+
+    var module = angular.module('app', [
+    	'ui.router',
+    	'ui.bootstrap',
+    	'app.home'
+    ]);
+
+    function config($stateProvider, $urlRouterProvider) {
+
+    	$stateProvider
+    		.state('app', {
+    			abstract: true,
+    			views: {
+    				'header@': {
+    					templateUrl: 'app/shared/header.html'
+    				},
+    				'footer@': {
+    					templateUrl: 'app/shared/footer.html'
+    				}
+    			}
+    		});
+
+    	$urlRouterProvider.otherwise('/');
+    }
+
+    module.config(config);
+})();
