6b39c35af38e30964e51b1db2748133270a9a559
Matt Preziotti <mattp22@vt.edu>
2015-07-06 11:59:40 -0400

    most content finished

--- public/app/philanthropy/philanthropy.ctrl.js
+++ public/app/philanthropy/philanthropy.ctrl.js
@@ -14,7 +14,32 @@
             "app/philanthropy/images/IMG_0468.PNG",
             "app/philanthropy/images/IMG_0469.PNG",
             "app/philanthropy/images/IMG_0470.PNG",
+            "app/philanthropy/images/tug1.jpg",
+            "app/philanthropy/images/tug2.jpg"
         ];
+
+        vm.info = [{
+            header: "Feeding America",
+            img: "app/philanthropy/images/falogo.jpg",
+            text: [
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad praesentium nostrum quidem omnis alias, iure nam eveniet laudantium enim dignissimos ratione, voluptas natus repudiandae, recusandae. Dolorum incidunt, sapiente explicabo voluptate!",
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem omnis qui doloribus vitae nam enim ipsum perferendis accusamus, odio sit similique deserunt, repudiandae inventore facere suscipit quia ullam sunt blanditiis."
+            ]
+        }, {
+            header: "Poster Contest",
+            img: "app/philanthropy/images/posters2.jpg",
+            text: [
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis non corrupti atque corporis beatae inventore. Voluptatum laboriosam animi vel dolorum repellendus, iusto, iure repellat quo eveniet eos, delectus odit cum.",
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam natus temporibus debitis voluptate sequi, voluptates, soluta, itaque quia nulla eos quod ipsum minima accusantium et, molestias odit est nesciunt modi?"
+            ]
+        }, {
+            header: "Field Games",
+            img: "app/philanthropy/images/watermelon.jpg",
+            text: [
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor asperiores laudantium dolore cupiditate, placeat facilis saepe aspernatur natus maiores corporis architecto ad velit. Obcaecati aperiam, placeat assumenda explicabo nulla, repellendus.",
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit eius, delectus atque molestias pariatur sapiente excepturi. Alias laudantium numquam, molestias, eos optio, cupiditate fuga dolore dolores voluptate quia voluptas quod!"
+            ]
+        }];
     }
 
     module.controller('PhilanthropyCtrl', PhilanthropyCtrl);
