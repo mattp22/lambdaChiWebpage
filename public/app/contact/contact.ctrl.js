4281ee64a6f53aff35f0fffb53096cfaafa58521
Matt Preziotti <mattp22@vt.edu>
2015-07-07 16:39:11 -0400

    added contact us

--- public/app/contact/contact.ctrl.js
+++ public/app/contact/contact.ctrl.js
@@ -1,45 +1,61 @@
 (function(module) {
 
-    function ContactCtrl($scope) {
+    function ContactCtrl($scope, $http) {
         var vm = this;
 
-        vm.info = [
-            {
-                name: "President - Paolo Guma",
-                phone: "908-255-0815",
-                email: "Paolo31@vt.edu"
-            },
-            {
-                name: "Internal Vice President - Blake Duncan",
-                phone: "571-926-2320",
-                email: "blake94@vt.edu"
-            },
-            {
-                name: "External Vice President - Christopher Connors",
-                phone: "703-254-4507",
-                email: "Cchri12@vt.edu"
-            },
-            {
-                name: "Treasurer - James Chapman",
-                phone: "484-643-2705",
-                email: "jamesc12@vt.edu"
-            },
-            {
-                name: "Rush Chair - Peter Burns",
-                phone: "804-855-8555",
-                email: "Pburns7@vt.edu"
-            },
-            {
-                name: "Rush Chair - Brian Heffernan",
-                phone: "864-612-8745",
-                email: "bheff@vt.edu"
-            },
-            {
-                name: "Alumni Secretary - Theo Jolley",
-                phone: "540-850-7911",
-                email: "tjolley7@vt.edu"
-            }
-        ];
+        vm.sendEmail = function() {
+
+            var data = {
+                contactName: vm.contactName,
+                contactEmail: vm.contactEmail,
+                subject: vm.subject,
+                message: vm.message
+            };
+
+            $http({
+                method: 'post',
+                url: '/api/emails',
+                data: data
+            }).
+            success(function(data, status, headers, config) {
+                alert('Your email has been sent.');
+                console.log(status);
+            }).
+            error(function(data, status, headers, config) {
+                alert('There was an error in submitting your email.');
+                console.log(status);
+            });
+        };
+
+        vm.info = [{
+            name: "President - Paolo Guma",
+            phone: "908-255-0815",
+            email: "Paolo31@vt.edu"
+        }, {
+            name: "Internal Vice President - Blake Duncan",
+            phone: "571-926-2320",
+            email: "blake94@vt.edu"
+        }, {
+            name: "External Vice President - Christopher Connors",
+            phone: "703-254-4507",
+            email: "Cchri12@vt.edu"
+        }, {
+            name: "Treasurer - James Chapman",
+            phone: "484-643-2705",
+            email: "jamesc12@vt.edu"
+        }, {
+            name: "Rush Chair - Peter Burns",
+            phone: "804-855-8555",
+            email: "Pburns7@vt.edu"
+        }, {
+            name: "Rush Chair - Brian Heffernan",
+            phone: "864-612-8745",
+            email: "bheff@vt.edu"
+        }, {
+            name: "Alumni Secretary - Theo Jolley",
+            phone: "540-850-7911",
+            email: "tjolley7@vt.edu"
+        }];
     }
 
     module.controller('ContactCtrl', ContactCtrl);
