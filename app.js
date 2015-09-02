1032609a49a675e68ed0f830ea8668abee73162c
Matt <mattp22@vt.edu>
2015-09-01 20:57:27 -0400

    self ping

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jsend = require('jsend');
+require('heroku-self-ping')("http://lambdachivt.herokuapp.com");
 
 var app = express();
 
